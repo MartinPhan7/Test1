2917b73ebcfb34788c15b9e01691b1c921faec9c
MartinPhan7 <MartinPhan7@github.com>
2022-05-13 00:00:47 -0400

    Commit from Apache

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom;
+import aleatoire.Person;
+/**
+ *
+ * @author pham07058706
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Mises a jour de BonjourNom
+        Person martin = new Person(args[0],args[1],Integer.parseInt(args[2]));
+        System.out.println("Bonjour "+martin.getFirstName()+" "+martin.getLastName()+ " "+martin.getAge());
+    }
+    
+}
